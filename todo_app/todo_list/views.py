dce38057145442d2198d4c4a4ae67f6440f46990
TS22082 <ts22082@gmail.com>
2020-02-29 12:32:47 -0800

    added next steps

--- todo_app/todo_list/views.py
+++ todo_app/todo_list/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import List
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    todo_list = List.objects.all
+    return render(request, 'home.html', {"todo_list" : todo_list})
