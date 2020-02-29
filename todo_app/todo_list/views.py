132797f1e291048f1f40fa2e2e2b4426b4794b46
TS22082 <ts22082@gmail.com>
2020-02-29 13:37:08 -0800

    next steps

--- todo_app/todo_list/views.py
+++ todo_app/todo_list/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
+from .forms import ListForm
 
 # Create your views here.
 
 def home(request):
-    todo_list = List.objects.all
-    return render(request, 'home.html', {"todo_list" : todo_list})
+
+    if request.method == "POST":
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+
+            todo_list = List.objects.all
+            return render(request, 'home.html', {"todo_list" : todo_list})
+
+    else:
+        todo_list = List.objects.all
+        return render(request, 'home.html', {"todo_list" : todo_list})
+
+def delete(request, todo_id):
+    item = List.objects.get(pk=todo_id)
+    item.delete()
+    return redirect('home')
